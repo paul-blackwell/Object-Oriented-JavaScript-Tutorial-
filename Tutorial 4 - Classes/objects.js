871c0707ff6ce2aa61d7cb3cd380634fc1fda1a4
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-24 10:45:47 +0000

    Tutorial #4 - Classes

---
+++ Tutorial 4 - Classes/objects.js
@@ -0,0 +1,15 @@
+
+/**
+ * Classes in JavaScript are like blueprints
+ * 
+ * For example a class that describes a car would have
+ * a color property and every car would have that property,
+ * so we define those properties in our class for the car
+ * "our blueprint for the car",. But we don't say what color 
+ * the car is because that is specific to each individual instance 
+ * of that class.
+ */
+
+ class User {
+     
+ }
