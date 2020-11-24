e68d248a6fb82cbb53bbcfd76a9896922394ef01
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-24 12:28:00 +0000

    Tutorial #8 - Class Inheritance

---
+++ Tutorial 8 - Class Inheritance/objects.js
@@ -0,0 +1,47 @@
+
+
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+
+    login() {
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+
+    logout() {
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore() {
+        this.score ++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+
+}
+
+class Admin extends User {
+
+    // Note: We will inherit the constructor from User, but we can add our own
+
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email !== user.email;
+        });
+    }
+    
+}
+
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shawn');
+
+var users = [userOne, userTwo, admin];
+
+admin.deleteUser(userTwo);
+console.log(users);
