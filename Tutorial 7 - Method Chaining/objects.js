5a1b73388bf5dad5f833e05fae084b6b8dd7dc08
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-24 12:13:09 +0000

    Tutorial #7 - Method Chaining

---
+++ Tutorial 7 - Method Chaining/objects.js
@@ -0,0 +1,33 @@
+
+
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+
+    login() {
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+
+    logout() {
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore() {
+        this.score ++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+
+
+// Method chaining 
+userOne.login().updateScore().updateScore().logout();
+
